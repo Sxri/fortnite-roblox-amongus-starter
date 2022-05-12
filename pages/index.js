ce0044082d45a351445975af18ad8b2c910e1565
Sxri <84330709+Sxri@users.noreply.github.com>
2022-05-12 13:38:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="welocome, here we murder men" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
